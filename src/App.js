38f2658283459907a47f4ae8fbaf8b13eff3b150
Dylan Nirvana <dylan@dylannirvana.com>
2022-03-29 13:18:06 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Button } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header id='home' className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,8 +18,17 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <h1>Have any questions?</h1>
+        <Button variant="secondary" href='#answer' >Well, how did I get here?</Button>{' '}
       </header>
+      <div id='answer'>
+        <h1>Here is an answer.</h1>
+        <p>Duis Lorem incididunt anim quis.</p>
+        <h3>Back to the top</h3>
+        <Button variant="secondary" href='#home' >Same as it ever was.</Button>{' '}
+
+      </div>
     </div>
   );
 }
